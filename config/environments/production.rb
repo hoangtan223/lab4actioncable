89b16f5f70761b3bc66f5b42fe37a325afde5ede
Tanbui <hoangtan223@gmail.com>
2016-07-27 20:29:08 +0700

    Add config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [
+  'https://ichat22392.herokuapp.com', 
+  'http://ichat22392.herokuapp.com'
+]
 end
